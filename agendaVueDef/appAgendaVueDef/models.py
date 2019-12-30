dfc4f4187c40f383665d874b0f83dca01a6e240d
nereasolabarrieta <nerea.solabarrieta@opendeusto.es>
2019-12-30 12:27:08 +0100

    a

--- agendaVueDef/appAgendaVueDef/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Contacto(models.Model):
-	# Campo para la relación one-to-many
-	nombre = models.CharField(('Nombre'),max_length=40)
-	email = models.CharField(('Email'),max_length=40)
-	telefono = models.CharField(('Telefono'),max_length=40)
-	def __str__(self):
-		return self.nombre
